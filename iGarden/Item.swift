fb79d3e5e6893ff8fa50d2fdc76c08df2099493b
aslekinnerod <asle.b.kinnerod@shortcut.no>
2026-08-21 09:59:37 +0200

    Initial Commit

---
+++ iGarden/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  iGarden
+//
+//  Created by Asle Kinnerød on 21/08/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
